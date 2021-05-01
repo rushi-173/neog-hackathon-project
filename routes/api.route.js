76e3f0f820bf5e212cd01bc323f5dfc151e2d9da
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-05-01 07:06:22 +0000

    feat: added socket.io and implemented basic broadcast functionality 📡

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,11 +1,19 @@
 const router = require("express").Router();
 
+router.use((req, res, next) => {
+  req.io.of("/api/chatroom").on('connection', (socket) => {
+    console.log("User connected " + socket.id);
+
+    socket.on("message", (data) => {
+      socket.broadcast.emit('message', data)
+    });
+  });
+  next();
+})
+
 
 router.get("/", (req, res) => {
-
-
   res.send("Welcome to api");
-
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,13 +6,28 @@ const cors = require('cors');
 const verify = require("./controllers/verifyToken");
 
 const app = express();
+const server = require('http').createServer(app);
 
+//socket io config
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+
+
+//middlewares
+function putIO(req, res, next) {
+  req.io = io;
+  next();
+}
 
 //use middlewares
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(putIO);
 
 //import routes
 const authRoute = require('./routes/auth.route');
@@ -29,14 +44,14 @@ app.all('/api/*', verify, function(req, res, next){
     next();
 });
 
-app.use('/api', verify, apiRoute);
+app.use('/api', apiRoute);
 
 //Connect to database
 dbConnect();
 
 //Listening to the server
 const port = process.env.PORT || 5000;
-const expressServer = app.listen(port, () => {
+const expressServer = server.listen(port, () => {
   console.log("server running...")
 });
 
